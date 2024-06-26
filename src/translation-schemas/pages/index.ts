2dce7b67ea66e54cfc4eed12a0e45c4e3d12bb11
valery-chumak <blossssom88@gmail.com>
2024-06-26 14:22:48 +0300

    changes service mock data

--- src/translation-schemas/pages/index.ts
+++ src/translation-schemas/pages/index.ts
@@ -1,21 +1,10 @@
-<<<<<<< HEAD
 import { PageTemplateId } from "../../enums/enums";
 import { servicePage } from "./service-page";
-import { developersPage } from "./developers-page";
-
-const pageIdToTranslationKeys = {
-  [PageTemplateId.SERVICE_PAGE]: servicePage,
-  [PageTemplateId.DEVELOPERS_PAGE]: developersPage,
-};
-=======
-import { PageTemplateId } from '../../enums/enums';
-import { servicePage } from './service-page';
-import { technologyPage } from './technology-page';
+import { technologyPage } from "./technology-page";
 
 const pageIdToTranslationKeys = {
   [PageTemplateId.SERVICE_PAGE]: servicePage,
   [PageTemplateId.TECHNOLOGY_PAGE]: technologyPage,
-}
->>>>>>> main
+};
 
 export { pageIdToTranslationKeys };
